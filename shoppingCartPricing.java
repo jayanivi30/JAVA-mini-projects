dd5aa55bdac0358471789ef85a55cb889a2ccda0
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2025-02-14 22:50:19 +0530

    Add files via upload

---
+++ shoppingCartPricing.java
@@ -0,0 +1,22 @@
+package Basics;
+import java.util.*;
+public class shoppingCartPricing{
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("What item did you buy? ");
+		String item;
+		item=sc.next();
+		System.out.println("What is the price of the item? ");
+		double price;
+		price=sc.nextDouble();
+		System.out.println("How many did you buy? ");
+		int num;
+		num=sc.nextInt();
+		double total;
+		total = price*num;
+		System.out.println("Your total bill amount for "+num+" "+item+" is Rs."+total);
+		sc.close();
+		
+	}
+	
+}
